5c734fb12a2f9addd13f6cd7c59080ab23746287
Max Thiele <fabmax.thiele@gmail.com>
2025-11-23 20:28:11 +0100

    Use JVM thread pool for Box2D multi-threading

--- box2d-native/src/webidlbindings/EnkiTS_integration.hpp
+++ box2d-native/src/webidlbindings/EnkiTS_integration.hpp
@@ -6,18 +6,45 @@
 #define MAX_TASKS 128
 #define THREAD_LIMIT 32
 
+static void* EnqueueTask(b2TaskCallback* box2dTask, int itemCount, int minRange, void* box2dContext, void* context);
+static void FinishTask(void* userTask, void* context);
+
+class TaskManager {
+public:
+	virtual ~TaskManager() = default;
+
+	void install(b2WorldDef& worldDef, int threadCount) {
+	    this->threadCount = threadCount;
+		worldDef.enqueueTask = EnqueueTask;
+		worldDef.finishTask = FinishTask;
+		worldDef.workerCount = threadCount;
+		worldDef.userTaskContext = this;
+	}
+
+    virtual void stepWorld(uint64_t worldId, float timeStep, int subStepCount) {
+        b2World_Step(b2LoadWorldId(worldId), timeStep, subStepCount);
+    }
+
+    void executeTask(uint32_t start, uint32_t end, uint32_t threadIndex, uint64_t box2dTask, uint64_t box2dContext) {
+        ((b2TaskCallback*) box2dTask)(start, end, threadIndex, (void*) box2dContext);
+    }
+
+    virtual uint64_t enqueueTask(uint64_t box2dTask, int itemCount, int minRange, uint64_t box2dContext) = 0;
+    virtual void finishTask(uint64_t userTask) = 0;
+
+    int threadCount;
+};
+
 typedef struct TaskData {
 	b2TaskCallback* box2dTask;
 	void* box2dContext;
 } TaskData;
 
 static void ExecuteRangeTask(uint32_t start, uint32_t end, uint32_t threadIndex, void* context);
-static void* EnqueueTask(b2TaskCallback* box2dTask, int itemCount, int minRange, void* box2dContext, void* context);
-static void FinishTask(void* userTask, void* context);
 
-class TaskManager {
+class EnkiTaskManager : TaskManager {
 public:
-    TaskManager(b2WorldDef& worldDef, int threadCount) : threadCount(threadCount) {
+    EnkiTaskManager(b2WorldDef& worldDef, int threadCount) {
         scheduler = enkiNewTaskScheduler();
         struct enkiTaskSchedulerConfig config = enkiGetTaskSchedulerConfig(scheduler);
         config.numTaskThreadsToCreate = threadCount - 1;
@@ -26,32 +53,27 @@ public:
         for (int taskIndex = 0; taskIndex < MAX_TASKS; taskIndex++) {
             tasks[taskIndex] = enkiCreateTaskSet(scheduler, ExecuteRangeTask);
         }
-
-        worldDef.enqueueTask = EnqueueTask;
-        worldDef.finishTask = FinishTask;
-        worldDef.workerCount = threadCount;
-        worldDef.userTaskContext = this;
+        install(worldDef, threadCount);
     }
 
-    ~TaskManager() {
+    virtual ~EnkiTaskManager() {
         for (int taskIndex = 0; taskIndex < MAX_TASKS; taskIndex++) {
             enkiDeleteTaskSet(scheduler, tasks[taskIndex]);
         }
         enkiDeleteTaskScheduler(scheduler);
     }
 
-    void stepWorld(uint64_t worldId, float timeStep, int subStepCount) {
-        b2World_Step(b2LoadWorldId(worldId), timeStep, subStepCount);
+    virtual void stepWorld(uint64_t worldId, float timeStep, int subStepCount) {
+        TaskManager::stepWorld(worldId, timeStep, subStepCount);
         taskCount = 0;
     }
 
-    enkiTaskSet* enqueueTask(b2TaskCallback* box2dTask, int itemCount, int minRange, void* box2dContext) {
-        //printf("Enqueue task %d, itemCount=%d, range=%d\n", taskCount, itemCount, minRange);
+    virtual uint64_t enqueueTask(uint64_t box2dTask, int itemCount, int minRange, uint64_t box2dContext) {
         if (taskCount < MAX_TASKS) {
             enkiTaskSet* task = tasks[taskCount];
             TaskData* data = taskData + taskCount;
-            data->box2dTask = box2dTask;
-            data->box2dContext = box2dContext;
+            data->box2dTask = (b2TaskCallback*) box2dTask;
+            data->box2dContext = (void*) box2dContext;
 
             struct enkiParamsTaskSet params;
             params.minRange = minRange;
@@ -63,15 +85,15 @@ public:
             enkiAddTaskSet(scheduler, task);
 
             ++taskCount;
-            return task;
+            return (uint64_t) task;
         } else {
             std::cerr << "[Box2D] Too many tasks enqueued! You should use TaskManager.stepWorld() to run a multi-threaded simulation" << std::endl;
-            box2dTask(0, itemCount, 0, box2dContext);
-            return NULL;
+            ((b2TaskCallback*) box2dTask)(0, itemCount, 0, (void*) box2dContext);
+            return 0L;
         }
     }
 
-    void finishTask(void* userTask) {
+    virtual void finishTask(uint64_t userTask) {
         enkiTaskSet* task = (enkiTaskSet*) userTask;
         enkiWaitForTaskSet(scheduler, task);
     }
@@ -80,7 +102,6 @@ private:
     enkiTaskScheduler* scheduler;
     enkiTaskSet* tasks[MAX_TASKS];
     TaskData taskData[MAX_TASKS];
-    int threadCount;
     int taskCount = 0;
 };
 
@@ -91,10 +112,10 @@ static void ExecuteRangeTask(uint32_t start, uint32_t end, uint32_t threadIndex,
 
 static void* EnqueueTask(b2TaskCallback* box2dTask, int itemCount, int minRange, void* box2dContext, void* context) {
     TaskManager* taskManager = (TaskManager*) context;
-    return taskManager->enqueueTask(box2dTask, itemCount, minRange, box2dContext);
+    return (void*) taskManager->enqueueTask((uint64_t) box2dTask, itemCount, minRange, (uint64_t) box2dContext);
 }
 
 static void FinishTask(void* userTask, void* context) {
     TaskManager* taskManager = (TaskManager*) context;
-    return taskManager->finishTask(userTask);
+    return taskManager->finishTask((uint64_t) userTask);
 }
